c3c3ad7c57ab48839d334a260f9575654752173d
Baraa Alfaris <baraa.ali1912@gmail.com>
2022-07-04 14:19:25 +0400

    adding more functions

--- RecipeExercise/Program.cs
+++ RecipeExercise/Program.cs
@@ -1,10 +1,13 @@
-﻿class Recipe
+﻿using System;
+using Spectre.Console;
+
+class Recipe
 {
     public string Title { get; set; }
     public string Instructions { get; set; }
     public List<string> Categories { get; set; }
     public string Ingredients { get; set; }
-    public int Id;
+    public Guid Id { get; set; }
 
     public Recipe(string title, string instructions, List<string> categories, string ingredients)
     {
@@ -32,13 +35,63 @@ class Operations
 
 
     }
+    public Recipe getRecipe(Guid id)
+    {
+        var recipe = Recipes.Find(rec => rec.Id == id);
+        if (recipe == null)
+        {
+            Console.WriteLine("Wrong Id input");
+            Environment.Exit(0);
+        }
+        return recipe;
+    }
+    public void EditInstruction(Guid id, string Newinstruction)
+    {
+        var recipe = getRecipe(id);
+        recipe.Instructions = Newinstruction;
+
+    }
+    public void EditTitle(Guid id , string NewTitle)
+    {
+        var recipe = getRecipe(id);
+        recipe.Title = NewTitle;
+    }
+
+    public void EditIngredient(Guid id , string NewIngredient)
+    {
+        var recipe = getRecipe(id);
+        recipe.Ingredients = NewIngredient;
+    }
+    public void RemoveCategory(Guid id, string category)
+    {
+        var recipe = getRecipe(id);
+        recipe.Categories.RemoveAll(c => c == category);
+    }
+    public void EditCategory(Guid id, string category, string newCategory)
+    {
+        RemoveCategory(id, category);
+        AddCategory(id, newCategory);
+    }
+    public void AddCategory(Guid id, string newCategory)
+    {
+        var recipe = getRecipe(id);
+        recipe.Categories.Add(newCategory);
+    }
     public void EditRecipe()
     {
         Console.WriteLine("Enter the Id of the Recipe you want to edit");
-        Console.WriteLine("What you would like to change ?");
+        Guid Id = Convert.ChangeType(Console.ReadLine(), Guid)
+;        Console.WriteLine("What you would like to change ?");
+        int Choice = Convert.ToInt32(Console.ReadLine());
+
         Console.WriteLine("1- Title");
         Console.WriteLine("2- Recipe Ingredient");
         Console.WriteLine("3- Recipe Instruction");
         Console.WriteLine("4- Recipe Categories");
+        if (Choice == 1) {
+            Console.WriteLine("Enter the new Title");
+            string NewTitle = Console.ReadLine();
+            EditTitle(Id, NewTitle);
+        } 
     }
 }
